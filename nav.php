20cc9d42adca93b2518c5182c549be8096b911e3
Al1374525 <albertayiteyadjin@gmail.com>
2023-10-05 02:08:02 -0400

    just got the files that i needed. will clean up later

---
+++ nav.php
@@ -0,0 +1,19 @@
+<nav>
+    <div class="menu">
+        <div class="logo">
+            <a href="https://www.cs.odu.edu/~410green/">Gamer Connection</a>
+        </div>
+        <ul>
+            <li><a href="https://www.cs.odu.edu/~410green/">Home</a></li>
+            <li><a href="team.html">Team</a></li>
+            <li><a href="problem.html">Problem</a></li>
+            <li><a href="solution.html">Solution</a></li>
+            <li><a href="deliverables.html">Deliverables</a></li>
+            <li><a href="presentations.html">Presentations</a></li>
+            <li><a href="glossary.html">Glossary</a></li>
+            <li><a href="references.html">References</a></li>
+
+
+        </ul>
+    </div>
+</nav>
