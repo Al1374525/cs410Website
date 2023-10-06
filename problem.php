07edc6253cea8a8902b229fbc89ac43ed2f665cb
Albert Ayitey-Adjin <aayit001@odu.edu>
2023-10-06 15:25:54 -0400

    Testing to see if this works

---
+++ problem.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset ="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title> GameFlash | Problem</title>
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+    </head>
+<body>
+    <?php include 'nav.php'; ?>
+    
+    <main class="content-wrapper">
+        <h1 class="page-title">The Problem</h1>
+        <section class="problem-description">
+            <p>
+                The challenge is the isolation and difficulty that Gamers
+                face in discovering new titles, while lacking a dedicated platform to connect with like-minded players.
+
+                Many Gamers find it hard to explore new games and build a community around their gaming interests,
+                leading to a sense of disconnection in an increasingly vast and diverse gaming landscape.
+                
+            </p>
+        </section>
+    </main>
+</body>
+</html>
