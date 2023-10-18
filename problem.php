feebd19b829ce26d0d09812f5dec64caf82aa0ad
Al1374525 <albertayiteyadjin@gmail.com>
2023-10-18 05:49:41 -0400

    ending workflow for tonight

--- problem.php
+++ problem.php
@@ -14,17 +14,20 @@
     <main class="content-wrapper">
         <h1 class="page-title">The Problem</h1>
         <section class="problem-description">
-            <br> <p>
-            Many gamers find it hard to explore new games and build a community around their gaming interests, leading to a sense of disconnection in an increasingly vast and diverse gaming landscape
-            </p></br>
-
-            <ul>
-                <li><strong>Diverse Game Library:</strong> Vast number of video games available, with thousands of options and different gaming styles.</li>
-                <li><strong>Finding Compatible Players:</strong> Identifying and connecting with players who match one's skill level, play style, and objectives.</li>
-                <li><strong>Tracking & Recognition:</strong> Difficulty in tracking and recognizing in-game achievements and trophies.</li>
-            </ul>
+            <h2>Problem Statement: </h2>
+            <p>Many gamers find it hard to explore new games and build a community around their gaming interests, leading to a sense of disconnection in an increasingly vast and diverse gaming landscape.</p>
         </section>
+        <h1 class="problem characteristics">Problem characteristics</h1>
+        <section class="problem-characteristics"> <!-- This div is used to potentially apply specific styles to the list -->
+                <ul style="list-style-type: disc; padding-left: 20px;">
+                    <li><strong>Diverse Game Library:</strong> Vast number of video games available, with thousands of options and different gaming styles.</li>
+                    <li><strong>Finding Compatible Players:</strong> Identifying and connecting with players who match one’s skill level, play style, and objectives.</li>
+                    <li><strong>Tracking & Recognition:</strong> Difficulty in tracking and recognizing in-game achievements and trophies.</li>
+                </ul>
+        </section>
+
         
+
     </main>
 </body>
 </html>
